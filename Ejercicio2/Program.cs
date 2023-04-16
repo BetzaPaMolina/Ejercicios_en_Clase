8e180781b7ec590623ccfc5007a7f91f5bf0b768
Betsabe Pa <lpam@miumg.edu.gt>
2023-04-15 21:07:16 -0600

    :white_check_mark: Ejercicio Finalizado

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Numeros pares e impares
+
+//Declaración de variables
+int numero;
+
+Console.WriteLine("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+Console.WriteLine("°            PARES E IMPARES              °");
+Console.WriteLine("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+//Solicitamos al usuario el ingreso del dato
+Console.WriteLine("Bienvenido, por favor Ingrese un numero:");
+numero = Convert.ToInt32(Console.ReadLine());
+//Determinamos los numeros pares
+if(numero % 2 == 0)
+{
+    Console.WriteLine($"Usted ingresó {numero}, corresponde a un numero par");
+}
+//determinamos los numeros impares
+else 
+{
+    Console.WriteLine($"Usted ingresó {numero}, corresponde a un numero impar");
+}
